13e625072520924d407d3b119b866f16fb2cbe7d
Mate Papp <matepapp@icloud.com>
2017-07-08 14:11:38 +0200

    Lesson 058 - Login form scaffolding.

---
+++ src/components/LoginForm.js
@@ -0,0 +1,22 @@
+// @flow
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Button, Card, CardSection } from './common';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection />
+        <CardSection />
+        <CardSection>
+          <Button>
+            Log in
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
