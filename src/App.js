b32ecca469e40f38c9379d49172195463076ea9e
Mate Papp <matepapp@icloud.com>
2017-07-08 14:02:14 +0200

    Lesson 057 - Initialized Firebase.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 // @flow
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDvy_CobrRuBG_UfSWTBviEgZY4TiaYOyc',
+      authDomain: 'reactnative-auth-90be3.firebaseapp.com',
+      databaseURL: 'https://reactnative-auth-90be3.firebaseio.com',
+      projectId: 'reactnative-auth-90be3',
+      storageBucket: 'reactnative-auth-90be3.appspot.com',
+      messagingSenderId: '103891811582'
+    });
+  }
+
   render() {
     return (
       <View>
